146b3a30f54b5a746422a0ba480213887a63b45a
willianwg <willian.wg70@gmail.com>
2022-08-07 16:39:04 -0300

    Adicionado redirecionamento para landing page ao clicar na logo de header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,13 @@
 import { styled } from "../stitches.config";
 import { FaUserCircle, FaShoppingCart } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 
 export function Header(){
     
     return(
            <Container>
-            <Logo>HORIZON.com</Logo>
+            <Link to="/" style ={{ textDecoration:"none", cursor:"none" }}><Logo>HORIZON.com</Logo></Link>
             <Links>
             <Click href="http://localhost:5173/singIn"><FaUserCircle size={23}/></Click>
             <Click href="https://amazon.com/"><FaShoppingCart size={23}/></Click>
@@ -29,7 +30,6 @@ const Logo = styled("h1",{
     height:20,
     fontSize:20,
     marginLeft:5,
-
 })
 
 const Links = styled("div",{
