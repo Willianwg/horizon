2ea3bdc30240a67793ba50614bfee34b8d1dfbe9
willianwg <willian.wg70@gmail.com>
2022-08-12 22:39:24 -0300

    Micro modificacoes na pagina checkout

--- frontend/src/pages/Checkout.tsx
+++ frontend/src/pages/Checkout.tsx
@@ -5,6 +5,31 @@ export function Checkout (){
     globalStyles();
     
     return (
-        <h1>Checkout Page</h1>
+        <Page>
+          <Container>
+          <h1>Checkout Page</h1>
+          <input />
+          <input />
+          <input />
+          <input />
+          <input />
+          </Container>
+        </Page>
     )
 }
+
+const Page = styled("div",{
+    display:"grid",
+    height:"100vh",
+    alignItems:"center",
+});
+
+const Container = styled("form",{
+    height:600,
+    width:"90%",
+    background:"white",
+    margin:"auto",
+    border:"1px solid grey",
+    padding:5,
+    
+});
