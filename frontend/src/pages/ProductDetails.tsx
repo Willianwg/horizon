9cbb7f553f71001a1df69c1ff44fd46bbf2ba026
Willian Guedes <willian.wg70@gmail.com>
2022-11-30 17:48:14 +0000

    improve product details page styles

--- frontend/src/pages/ProductDetails.tsx
+++ frontend/src/pages/ProductDetails.tsx
@@ -1,102 +1,123 @@
 import { useEffect, useState } from "react";
-import { styled, globalStyles} from "../stitches.config";
+import { styled, globalStyles } from "../stitches.config";
 import { Header } from "../components/Header";
 import { useApi } from "../services/api";
 import { useLocation, useNavigate } from "react-router-dom";
 import { AiFillStar } from "react-icons/ai";
 import { Button } from "../styles/styles";
-export function Details (){
+export function Details() {
     const api = useApi();
     const navigate = useNavigate();
     const location = useLocation();
     const id = getIdFromUrl();
-    
+
     const [product, setProduct] = useState({});
-    
-    useEffect(()=>{
-        
-        async function loadProduct(){
+
+    useEffect(() => {
+
+        async function loadProduct() {
             const data = await api.getProductDetails(id);
-            
+
             setProduct(data);
         }
-        
-       loadProduct();
-       
-    },[])
-    
-    function getIdFromUrl(){
-        const parts=location.pathname.split("/");
-        const productId = parts[parts.length-1];
+
+        loadProduct();
+
+    }, [])
+
+    function getIdFromUrl() {
+        const parts = location.pathname.split("/");
+        const productId = parts[parts.length - 1];
         return productId;
     }
-    
-    
+
+
     globalStyles();
-    
+
     return (
         <Page>
-        <Header/>
-        <Container>
-         <ProductName>Test</ProductName>
-                        <Stars>
-                <AiFillStar color="orange"/>
-                <AiFillStar color="orange"/>
-                <AiFillStar color="orange"/>
-                <AiFillStar color="orange"/>
-               </Stars>
-         <Image style={{ backgroundImage:`url(http://localhost:3000/files/${product.image})` }}/>
-         <Price><C>R$</C>{product.price}</Price>
-         <Description>
-           <p>Descrição:</p>
-           <p>Accusantium quo aspernatur expedita rerum assumenda quaerat quisquam officiis, excepturi, inventore recusandae reprehenderit, corporis repellendus adipisci! Natus a aliquam alias ea. Placeat. </p>
-         </Description>
-         <Button color="dark" onClick={e=>navigate(`${location.pathname}/checkout`)}>Comprar</Button>
-         <Button>Adicionar ao carrinho</Button>
-        </Container>
+            <Header />
+            <Container>
+                <Image style={{ backgroundImage: `url(http://localhost:3000/files/${product.image})` }} />
+                <Right>
+                    <ProductName>Test</ProductName>
+                    <Stars>
+                        <AiFillStar color="orange" />
+                        <AiFillStar color="orange" />
+                        <AiFillStar color="orange" />
+                        <AiFillStar color="orange" />
+                    </Stars>
+                    <Price><C>R$</C>{55.99}</Price>
+                    <Description>
+                        <p>Descrição:</p>
+                        <p>Accusantium quo aspernatur expedita rerum assumenda quaerat quisquam officiis, excepturi, inventore recusandae reprehenderit, corporis repellendus adipisci! Natus a aliquam alias ea. Placeat. </p>
+                    </Description>
+                    <Button color="dark" onClick={e => navigate(`${location.pathname}/checkout`)}>Comprar</Button>
+                    <Button>Adicionar ao carrinho</Button>
+                </Right>
+            </Container>
         </Page>
     )
 }
 
 
-const Stars = styled("div",{
-   
+const Stars = styled("div", {
+
 });
 
-const Page = styled("div",{
-   
+const Page = styled("div", {
+
 });
 
-const Container = styled("div",{
-    boxSizing:"border-box",
-    margin:20,
-    padding:10,
-    display:"flex",
-    flexDirection:"column",
-    alignItems:"flex-start",
-    justifyContent:"flex-start",
-    background:'red',
-    height:'100vh',
+const Container = styled("div", {
+    boxSizing: "border-box",
+    margin: 15,
+    padding: 20,
+    display: 'grid',
+    gridTemplateColumns: '70fr 30fr',
+    gridGap: '10px',
+    '@sm': {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
+        justifyContent: "flex-start",
+    }
 });
 
-const Image = styled("img",{
-   width:'100%',
-   height:'100%',
+const Image = styled("img", {
+    width: '100%',
+    height: 500,
+    '@sm':{
+        height:300,
+    }
 });
 
-const ProductName = styled("h4", {
-   
+const ProductName = styled("h2", {
+    margin:0,
 })
 
 
 const Price = styled("h3", {
-   
+    margin:0,
+    fontSize:'25pt',
+    fontWeight:500,
 })
 
 const Description = styled("p", {
-   
+    '@md':{
+        fontSize:'14px'
+    }
 })
 
 const C = styled("span", {
-   
+
 })
+
+const Right = styled("div", {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "space-between",
+})
+
+
--- frontend/src/styles/styles.tsx
+++ frontend/src/styles/styles.tsx
@@ -12,8 +12,19 @@ export const Input = styled("input",{
 });
 
 export const Button = styled("button",{
-   
-   // background:"rgb(100,190,250)",
+   width:"100%",
+   color:'white',
+   fontWeight:900,
+   fontSize:14,
+   height:60,
+   borderRadius:5,
+   borderWidth:0,
+   alignSelf:'flex-end',
+   background:"rgb(100,190,250)",
+   cursor:'pointer',
+   '@sm':{
+        marginBottom:5
+   },
     variants:{
         size:{
            small:{
@@ -22,7 +33,7 @@ export const Button = styled("button",{
         },
         color:{
             dark:{
-                // background:"rgb(100,150,250)",
+                background:"rgb(100,150,250)",
             }
         }
     }
