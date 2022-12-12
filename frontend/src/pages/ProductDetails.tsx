432bc0c4398ca6e32007555743964329ae9534b2
Willian <willian.wg70@gmail.com>
2022-12-12 16:50:18 -0300

    feat: scroll to top when change to productDetails page

--- frontend/src/pages/ProductDetails.tsx
+++ frontend/src/pages/ProductDetails.tsx
@@ -31,7 +31,7 @@ export function Details() {
     });
 
     useEffect(() => {
-
+        window.scrollTo(0,0);
         async function loadProduct() {
             const data = await api.getProductDetails(id);
 
